70432fb7af553288279fc07bc4fdc33fcbe7e63f
TarasovAndy <ktn-at@mail.ru>
2019-09-16 09:46:37 +0300

    upd

--- SecureParcel/Controllers/HomeController.cs
+++ SecureParcel/Controllers/HomeController.cs
@@ -16,7 +16,54 @@ namespace SecureParcel.Controllers
             //SmartContract.Deploy();
 
             if (User.Identity.IsAuthenticated)
-                return RedirectToAction("Index", "SendParcel");
+                //return RedirectToAction("Index", "SendParcel");
+                return RedirectToAction("ProfileAccount", "SendParcel");
+            else
+                return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult AllShipments()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("AllShipments", "SendParcel");
+            else
+                return RedirectToAction("Login", "Account");
+
+            //ViewBag.ThisShipments_FixLayout = true;
+            //return View();
+        }
+
+        public ActionResult ProfileAccount()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("ProfileAccount", "SendParcel");
+            else
+                return RedirectToAction("Login", "Account");
+
+            //ViewBag.ThisShipments_FixLayout = true;
+            //return View();
+        }
+
+        public ActionResult SentShipment()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("SentShipment", "SendParcel");
+            else
+                return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult SentShipment_send()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("SentShipment_send", "SendParcel");
+            else
+                return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult SentShipment_Demo()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("SentShipment_Demo", "SendParcel");
             else
                 return RedirectToAction("Login", "Account");
         }
--- SecureParcel/Controllers/SendParcelController.cs
+++ SecureParcel/Controllers/SendParcelController.cs
@@ -18,71 +18,189 @@ namespace SecureParcel.Controllers
     {
         private DatabaseContext dbContext = new DatabaseContext();
 
+        //[AllowAnonymous]
+        //public async Task<ActionResult> Index()
+        //{
+        //    var token = await DhlApi.GetToken();
+        //    var item = await DhlApi.AddItem();
+
+
+        //    var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+
+        //    if (identity == null || identity.Name == null)
+        //    {
+        //        return RedirectToAction("Login", "Account");                
+        //    }
+        //    else
+        //    {
+        //        var user = dbContext.Users.FirstOrDefault(x => x.Email == identity.Name);
+
+        //        if (user == null)
+        //        {
+        //            return RedirectToAction("Login", "Account");
+        //        }
+        //        else if (!user.IsActivated)
+        //        {
+        //            return RedirectToAction("WaitActivation", "Account");
+        //        }
+
+        //        var model = new ParcelModel();
+        //        var senderList = new List<Parcel>();
+        //        var recipientList = new List<Parcel>();
+        //        var parcels = new List<Parcel>();
+
+        //        using (var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new DatabaseContext())))
+        //        {
+        //            var userId = userManager.FindByName(identity.Name).Id;
+        //            var publicKey = user.PublicKey;
+
+        //            if (userManager.IsInRole(userId, "Admin"))
+        //            {
+        //                parcels = dbContext.Parcels.Include("Sender").ToList();
+        //            }
+        //            else
+        //            {
+        //                parcels = dbContext.Parcels.Include("Sender").Where(x => x.Sender.Id == userId || x.RecipientPublicKey == publicKey).ToList();
+        //            }
+
+        //            foreach (var parcel in parcels)
+        //            {
+        //                if (userManager.IsInRole(userId, "Admin") || parcel.Sender.Id == userId)
+        //                {
+        //                    model.ParcelList.Add(new ParcelModel.TableItem()
+        //                    {
+        //                        GUID = parcel.GUID,
+        //                        Amount = parcel.PaymentAmount?.ToString() + " CS",
+        //                        ParcelName = parcel.Name,
+        //                        RecipientName = string.IsNullOrEmpty(parcel.RecipientName) ? parcel.RecipientPublicKey : parcel.RecipientName,
+        //                        SenderName = string.IsNullOrEmpty(parcel.Sender?.FullName) ? parcel.Sender?.PublicKey : parcel.Sender?.FullName,
+        //                        Status = parcel.DeliveryStatus,
+        //                        CreatedAt = parcel.CreatedAt
+        //                    });
+        //                }
+        //            }
+        //        }
+
+        //        return View(model);
+        //    }
+        //}
+
         [AllowAnonymous]
-        public async Task<ActionResult> Index()
+        public ActionResult AllShipments()
         {
-            var token = await DhlApi.GetToken();
-            var item = await DhlApi.AddItem();
-
-
             var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
-
             if (identity == null || identity.Name == null)
             {
-                return RedirectToAction("Login", "Account");                
+                return RedirectToAction("Login", "Account");
             }
             else
             {
-                var user = dbContext.Users.FirstOrDefault(x => x.Email == identity.Name);
-
-                if (user == null)
-                {
-                    return RedirectToAction("Login", "Account");
-                }
-                else if (!user.IsActivated)
-                {
-                    return RedirectToAction("WaitActivation", "Account");
-                }
-
-                var model = new ParcelModel();
-                var senderList = new List<Parcel>();
-                var recipientList = new List<Parcel>();
-                var parcels = new List<Parcel>();
-
-                using (var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new DatabaseContext())))
-                {
-                    var userId = userManager.FindByName(identity.Name).Id;
-                    var publicKey = user.PublicKey;
-
-                    if (userManager.IsInRole(userId, "Admin"))
-                    {
-                        parcels = dbContext.Parcels.Include("Sender").ToList();
-                    }
-                    else
-                    {
-                        parcels = dbContext.Parcels.Include("Sender").Where(x => x.Sender.Id == userId || x.RecipientPublicKey == publicKey).ToList();
-                    }
-
-                    foreach (var parcel in parcels)
-                    {
-                        if (userManager.IsInRole(userId, "Admin") || parcel.Sender.Id == userId)
-                        {
-                            model.ParcelList.Add(new ParcelModel.TableItem()
-                            {
-                                GUID = parcel.GUID,
-                                Amount = parcel.PaymentAmount?.ToString() + " CS",
-                                ParcelName = parcel.Name,
-                                RecipientName = string.IsNullOrEmpty(parcel.RecipientName) ? parcel.RecipientPublicKey : parcel.RecipientName,
-                                SenderName = string.IsNullOrEmpty(parcel.Sender?.FullName) ? parcel.Sender?.PublicKey : parcel.Sender?.FullName,
-                                Status = parcel.DeliveryStatus,
-                                CreatedAt = parcel.CreatedAt
-                            });
-                        }
-                    }
-                }
-
-                return View(model);
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
             }
         }
+
+        [AllowAnonymous]
+        public ActionResult SentShipment()
+        {
+            var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null || identity.Name == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult SentShipment_send()
+        {
+            var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null || identity.Name == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult ProfileAccount()
+        {
+            var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null || identity.Name == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult SentShipment_Demo()
+        {
+            var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null || identity.Name == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
+            }
+        }
+
+        public ActionResult SentShipment_Demo_2()
+        {
+            var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null || identity.Name == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
+            }
+        }
+
+        public ActionResult SentShipment_Demo_3()
+        {
+            var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null || identity.Name == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
+            }
+        }
+
+        public ActionResult SentShipment_Demo_4()
+        {
+            var identity = HttpContext.GetOwinContext().Authentication.GetExternalIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null || identity.Name == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                ViewBag.ThisShipments_FixLayout = true;
+                return View();
+            }
+        }
+
     }
 }
